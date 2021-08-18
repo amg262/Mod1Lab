21ee8750f1c3fcb96659ea5099044ccf0071a6e5
Andrew <andrewmgunn26@gmail.com>
2021-08-18 14:26:23 -0500

    initial commit of the SUBMITTED ASSIGNMENT . THIS IS THE CORRECT ONE

---
+++ Mod1Lab/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+/*
+ * 
+ */
+namespace Mod1Lab
+{
+    
+    /*
+     * Class that contains the needed methods in addition to the main
+     * method running on console running. Contains 2 methods for Lab A
+     * and Lab B that get user parameters, convert them to correct data type
+     * used for math used below that methods calculate.
+     */
+    class Program
+    {
+
+
+        /*
+         * LabB runs math on a recipe based on input number that
+         * will dictate how the ratios will be by have pre-made ratios
+         *
+         * @return null
+         */
+        public string LabB()
+        {
+
+            /*
+            * assigning variables
+            */
+            double cupcakes = 8;
+            double eggs = 4 / cupcakes;
+            double sugar = 1 / cupcakes;
+            double cheese = 3 / cupcakes;
+            double vanilla = 1 / cupcakes;
+            
+            
+            /*
+             * get console input and convert to double for math
+             */
+            Console.WriteLine("Enter Number Cupcakes:");
+            string numCupcakes = Console.ReadLine();
+            double numCakes = Convert.ToDouble(numCupcakes);
+
+            
+
+            /*
+             * assigning variables and validating input
+             */
+            double n0 = numCakes * 1 / 2;
+            double n1 = numCakes * 1 /  8;
+            double n2 = numCakes * 3 / 8;
+            double n4 = numCakes * 1 / 8;
+            double numEggs = numCupcakes != null ?  numCakes * 1 / 2 : numCakes = 0;
+            double numSugar = numCupcakes != null ?  numCakes * 1 / 8 : numCakes = 0;
+            double numCheese = numCupcakes != null ?  numCakes * 3 / 8 : numCakes = 0;
+            double numVanilla = numCupcakes != null ?  numCakes * 1 / 8 : numCakes = 0;
+
+   
+            /*
+             * output the variables assigned from ratio math
+             */
+             Console.Write(numEggs);
+             Console.WriteLine(" eggs");
+             Console.Write(numSugar);
+             Console.WriteLine(" cups sugar");
+             Console.Write(numCheese);
+             Console.WriteLine(" packages of cream cheese");
+             Console.Write(numVanilla);
+             Console.WriteLine(" teaspoon vanilla");
+       
+             
+             /*
+             * return null just run and print
+             */
+             return null;
+        }
+        
+        /*
+         * Lab A runs the math done on the input values
+         * Takes 2 input numbers and finds product, sum, diff
+         *
+         * @return null
+         */
+        public string LabA()
+        {
+         
+            /*
+             * Getting console input of 2 variables used below
+             */
+            Console.WriteLine("Enter Number 1:");
+            string numOne = Console.ReadLine();
+            Console.WriteLine("Enter Number 2:");
+            string numTwo = Console.ReadLine();
+
+            /*
+            * assigning variables and converting for math below
+            */
+            int numOneInt = Convert.ToInt32(numOne);
+            int numTwoInt = Convert.ToInt32(numTwo);
+
+            /*
+            * validating input and writeing the math output
+            */
+            if ((numOne != null) && numTwo != null)
+            {
+             
+                Console.Write(numOneInt + numTwoInt);
+                Console.WriteLine(" sum");
+                Console.Write(numOneInt - numTwoInt);
+                Console.WriteLine(" difference");
+                Console.Write(numOneInt * numTwoInt);
+                Console.WriteLine(" product");
+            }
+            /*
+             * return null just run and print
+             */
+            return null;
+        }
+        /*
+         * main console method that instantiates a new Program class
+         * and runs LabA and LabB methods that will use console.writeline
+         * to output the labs required parameters and functions.
+         */
+        static void Main(string[] args)
+        {
+            /*
+             * create new class and run both methods
+             */
+            Program A = new Program();
+            A.LabA();
+            A.LabB();
+        }
+    }
+}
